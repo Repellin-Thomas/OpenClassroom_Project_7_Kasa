d9544ca1f334623d2ab58d035ffb2d1796a56e6e
Thomas Repellin <repellin.thomas8@gmail.com>
2023-04-15 21:40:33 +0200

    tre fix header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import ErrorPage from './pages/Error';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import HouseComponent from './pages/House';
 import './App.css'
 
 function App() {
     return (<div className="app-container">
+        <Header />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/a-propos" element={<About />} />
